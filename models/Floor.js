ab44d7dbe65595c433f9c211fdf814ff6ebde078
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-11-02 23:37:36 -0400

    Added Model class, and Floor class content, also installed mysql2 package

--- models/Floor.js
+++ models/Floor.js
@@ -1,5 +1,8 @@
-class Floor {
+const Model = require("./Model.js");
+
+class Floor extends Model {
   constructor(flr_id, flr_num) {
+    super("floor");
     this.flr_id = flr_id;
     this.flr_num = flr_num;
   }
