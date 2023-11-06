6f3c0c629265e4b70a4ea9fd91ed72130f689d98
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-11-06 13:16:08 -0500

    Added sub folders in css

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -1,6 +1,6 @@
 const allGuestView = (req, res) => {
   const pageTitle = "King William's - All Guests";
-  const pageStyle = "/css/all-guests.css";
+  const pageStyle = "/css/guest/all-guests.css";
   res.render("guest/all-guests", {
     pageTitle: pageTitle,
     pageStyle: pageStyle,
@@ -9,7 +9,7 @@ const allGuestView = (req, res) => {
 
 const addGuestView = (req, res) => {
   const pageTitle = "King William's - Add Guest";
-  const pageStyle = "/css/add-guest.css";
+  const pageStyle = "/css/guest/add-guest.css";
   res.render("guest/add-guest", {
     pageTitle: pageTitle,
     pageStyle: pageStyle,
--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,6 @@
 const addServiceView = (req, res) => {
   const pageTitle = "King William's - Add Service";
-  const pageStyle = "/css/add-service.css";
+  const pageStyle = "/css/service/add-service.css";
   res.render("service/add-service", {
     pageTitle: pageTitle,
     pageStyle: pageStyle,
