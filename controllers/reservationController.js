1cc5693555c0384000dab2c50c76180706dd1a54
Yuto Uogata <113719404+yuto916@users.noreply.github.com>
2023-11-19 19:25:06 -0500

    Added functionality for viewing the reservation and filtering

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -1,10 +1,19 @@
+const Reservation = require('../models/Reservation');
+
 const allReservationView = (req, res) => {
+  const criteria = req.query;
   const pageTitle = "King William's - Reservations";
   const pageStyle = "/css/reservation/all-reservations.css";
-  res.render("reservation/all-reservations", {
-    pageTitle: pageTitle,
-    pageStyle: pageStyle,
-  });
+
+  Reservation.findByCriteria(criteria)
+    .then(([rows]) => {
+      res.render("reservation/all-reservations", {
+        pageTitle: pageTitle,
+        pageStyle: pageStyle,
+        reservation: rows
+      });
+    })
+    .catch(err => res.status(500).send(err));
 };
 
 const addReservationView = (req, res) => {
---
+++ models/Reservation.js
@@ -0,0 +1,25 @@
+const pool = require('../database'); 
+
+class Reservation {
+  static findByCriteria(criteria) {
+    let query = "SELECT * FROM guest_stay inner join guest on guest_stay.guest_id=guest.guest_id inner join room on guest_stay.rm_id=room.rm_id";
+    let conditions = [];
+    let values = [];
+
+    Object.keys(criteria).forEach(key => {
+      if (criteria[key]) {
+        conditions.push(`${key} = ?`);
+        values.push(criteria[key]);
+      }
+    });
+
+    if (conditions.length) {
+      query += ' WHERE ' + conditions.join(' AND ');
+    }
+
+    return pool.promise().query(query, values);
+  }
+};
+
+
+module.exports = Reservation;
