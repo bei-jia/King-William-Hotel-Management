a9354e59f19241e68bc05808d21343065aa61dcc
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-11-20 10:25:30 -0500

    Updated guest, reservation, and all-employee css

--- database.js
+++ database.js
@@ -2,10 +2,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "1201_Bear",
+  user: "user1",
+  password: "password",
   database: "king_william_db",
 });
 
 module.exports = pool;
-
