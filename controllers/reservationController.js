2a46dfc37a13fdbc344fb266966851d2fc2f558c
Yuto Uogata <113719404+yuto916@users.noreply.github.com>
2023-12-04 18:55:08 -0500

    Completed room option dropdown for addReservation and occupying the room once reserved

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -16,15 +16,25 @@ const allReservationView = (req, res) => {
     .catch((err) => res.status(500).send(err));
 };
 
+
 const addReservationView = (req, res) => {
   const id = req.params.id;
-  Reservation.findGuestById(id)
-    .then(([rows]) => {
-      if (rows.length > 0) {
+
+  // Use Promise.all to fetch both guest and empty rooms in parallel
+  Promise.all([
+    Reservation.findGuestById(id),
+    Reservation.findEmptyRooms(),
+  ])
+    .then(([guestResult, emptyRoomsResult]) => {
+      const [guestRows] = guestResult;
+      const [emptyRoomsRows] = emptyRoomsResult;
+
+      if (guestRows.length > 0) {
         res.render("reservation/add-reservation", {
           pageTitle: "King William's - Add Reservation",
           pageStyle: "/css/reservation/add-reservation.css",
-          guest: rows[0],
+          guest: guestRows[0],
+          emptyRooms: emptyRoomsRows,
         });
       } else {
         res.status(404).send("Guest not found");
@@ -33,20 +43,50 @@ const addReservationView = (req, res) => {
     .catch((err) => res.status(500).send(err));
 };
 
-const addReservation = (req, res) => {
-  const guestId = req.params.id;
-  const newReservation = {
-    checkInDate: req.body.checkInDate,
-    checkOutDate: req.body.checkOutDate,
-    balance: 0,
-    isCancelled: 0,
-    cancelledTime: null,
-    guestId: guestId,
-    roomId: req.body.roomId,
-  };
 
-  Reservation.addReservation(newReservation)
-    .then(() => res.redirect("/reservation/all-reservations"))
-    .catch((err) => res.status(500).send(err.message));
+
+const addReservation = async (req, res) => {
+  try {
+    const guestId = req.params.id;
+
+    // Create a new reservation
+    const newReservation = {
+      checkInDate: req.body.checkInDate,
+      checkOutDate: req.body.checkOutDate,
+      balance: 0,
+      isCancelled: 0,
+      cancelledTime: null,
+      guestId: guestId,
+      roomId: req.body.roomId,
+    };
+
+    // Call a method to add the reservation
+    await Reservation.addReservation(newReservation);
+
+    // Update the room status to rm_is_occupied = 1
+    const roomUpdateSuccess = await Reservation.occupyRoom(req.body.roomId);
+
+    if (roomUpdateSuccess) {
+      // Redirect or send a response indicating success
+      res.redirect("/reservation/all-reservations");
+    } else {
+      // Handle the case where the room was not found or update failed
+      res.status(404).send('Room not found or failed to update room status');
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle errors and send an appropriate error response
+    res.status(500).send(error.message);
+  }
 };
+
+
+
+
+// Attach an 'uncaughtException' event handler to log uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  // Handle or log the uncaught exception here
+});
+
 module.exports = { allReservationView, addReservationView, addReservation };
