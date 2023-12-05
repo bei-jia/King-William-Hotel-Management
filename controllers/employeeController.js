7b73b1bbfa823a4ffedd91ffe11d06fe16b16fe6
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-12-04 23:46:10 -0500

    Added edit employee functionality

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,5 +1,5 @@
-
-const Employee = require('../models/Employee/Employee'); 
+const pool = require("../database");
+const Employee = require("../models/Employee/Employee");
 
 const allEmployeesView = (req, res) => {
   const criteria = req.query;
@@ -8,10 +8,10 @@ const allEmployeesView = (req, res) => {
       res.render("employee/all-employees", {
         pageTitle: "King William's - All Employees",
         pageStyle: "/css/employee/all-employees.css",
-        employees: rows
+        employees: rows,
       });
     })
-    .catch(err => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err));
 };
 
 const addEmployeeView = (req, res) => {
@@ -24,8 +24,7 @@ const addEmployeeView = (req, res) => {
 };
 
 const addEmployee = (req, res) => {
-  
-  const employmentType = req.body.type === 'permanent' ? 1 : 0;
+  const employmentType = req.body.type === "permanent" ? 1 : 0;
   const positionId = parseInt(req.body.position);
 
   const newEmployee = {
@@ -39,12 +38,94 @@ const addEmployee = (req, res) => {
     email: req.body.email,
     phone: req.body.phone,
     employmentType: employmentType,
-    positionId: positionId
+    positionId: positionId,
   };
 
   Employee.add(newEmployee)
-    .then(() => res.redirect('/employee/all-employees'))
-    .catch(err => res.status(500).send(err.message));
+    .then(() => res.redirect("/employee/all-employees"))
+    .catch((err) => res.status(500).send(err.message));
 };
 
-module.exports = { allEmployeesView, addEmployeeView, addEmployee };
+const editEmployeeView = (req, res) => {
+  const id = req.params.id;
+  const pageTitle = "King William's - Edit Employee";
+  const pageStyle = "/css/employee/edit-employee.css";
+
+  Employee.findByCriteria({ emp_id: id })
+    .then(([rows]) => {
+      if (rows.length > 0) {
+        res.render("employee/edit-employee", {
+          pageTitle: pageTitle,
+          pageStyle: pageStyle,
+          employee: rows[0],
+        });
+      } else {
+        res.status(404).send("Employee not found");
+      }
+    })
+    .catch((err) => res.status(500).send(err.message));
+};
+
+const editEmployee = (req, res) => {
+  const id = req.params.id;
+  const employmentType = req.body.type === "permanent" ? 1 : 0;
+  const positionId = parseInt(req.body.position);
+
+  const updatedEmployee = {
+    emp_fname: req.body.firstName,
+    emp_lname: req.body.lastName,
+    emp_hire_date: req.body.hiredDate,
+    emp_street: req.body.street,
+    emp_city: req.body.city,
+    emp_country: req.body.country,
+    emp_postal_code: req.body.postalCode,
+    emp_email: req.body.email,
+    emp_phone: req.body.phone,
+    emp_is_permanent: employmentType,
+    position_id: positionId,
+  };
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+
+    connection.beginTransaction((err) => {
+      if (err) {
+        connection.release();
+        res.status(500).send(err.message);
+        return;
+      }
+
+      Employee.updateById(id, updatedEmployee, connection)
+        .then(() => {
+          connection.commit((err) => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+                res.status(500).send(err.message);
+              });
+            } else {
+              connection.release();
+              res.redirect("/employee/all-employees");
+            }
+          });
+        })
+        .catch((err) => {
+          connection.rollback(() => {
+            connection.release();
+            res.status(500).send(err.message);
+          });
+        });
+    });
+  });
+};
+
+module.exports = {
+  allEmployeesView,
+  addEmployeeView,
+  addEmployee,
+  editEmployeeView,
+  editEmployee,
+};
--- models/Employee/Employee.js
+++ models/Employee/Employee.js
@@ -1,42 +1,38 @@
-
-const pool = require('../../database');
+const pool = require("../../database");
 
 class Employee {
-    static add(newEmployee) {
-      return new Promise((resolve, reject) => {
-        const query = `
+  static add(newEmployee) {
+    return new Promise((resolve, reject) => {
+      const query = `
           INSERT INTO employee (
             emp_fname, emp_lname, emp_hire_date, emp_street, emp_city, emp_country,
             emp_postal_code, emp_email, emp_phone, emp_is_permanent, position_id
           ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         `;
-        // Prepare the values to be inserted, including converting employment type to boolean
-        const values = [
-          newEmployee.firstName,
-          newEmployee.lastName,
-          newEmployee.hiredDate,
-          newEmployee.street,
-          newEmployee.city,
-          newEmployee.country,
-          newEmployee.postalCode,
-          newEmployee.email,
-          newEmployee.phone,
-          newEmployee.employmentType === 'permanent' ? 1 : 0,
-          newEmployee.positionId
-        ];
+      // Prepare the values to be inserted, including converting employment type to boolean
+      const values = [
+        newEmployee.firstName,
+        newEmployee.lastName,
+        newEmployee.hiredDate,
+        newEmployee.street,
+        newEmployee.city,
+        newEmployee.country,
+        newEmployee.postalCode,
+        newEmployee.email,
+        newEmployee.phone,
+        newEmployee.employmentType === "permanent" ? 1 : 0,
+        newEmployee.positionId,
+      ];
 
-        pool.query(query, values, (err, results) => {
-          if (err) reject(err);
-          else resolve(results.insertId); // assuming you might want the inserted employee's ID
-        }        
-        );
-        
+      pool.query(query, values, (err, results) => {
+        if (err) reject(err);
+        else resolve(results.insertId); // assuming you might want the inserted employee's ID
       });
-    }
+    });
+  }
 
-    static findByCriteria(criteria) 
-    {
-      let query = `
+  static findByCriteria(criteria) {
+    let query = `
         SELECT 
           employee.*, 
           position.position_desc
@@ -45,23 +41,40 @@ class Employee {
         LEFT JOIN 
           position ON employee.position_id = position.position_id
       `;
-      let conditions = [];
-      let values = [];
-  
-      Object.keys(criteria).forEach(key => {
-        if (criteria[key] && criteria[key] !== 'Any') {
-          conditions.push(`employee.${key} = ?`);
-          values.push(criteria[key]);
-        }
-      });
-  
-      if (conditions.length) {
-        query += ' WHERE ' + conditions.join(' AND ');
+    let conditions = [];
+    let values = [];
+
+    Object.keys(criteria).forEach((key) => {
+      if (criteria[key] && criteria[key] !== "Any") {
+        conditions.push(`employee.${key} = ?`);
+        values.push(criteria[key]);
       }
-  
-      return pool.promise().query(query, values);
+    });
+
+    if (conditions.length) {
+      query += " WHERE " + conditions.join(" AND ");
     }
 
+    return pool.promise().query(query, values);
+  }
+
+  static updateById(id, updateData, connection) {
+    let query = "UPDATE employee SET ";
+    let updates = [];
+    let values = [];
+
+    Object.keys(updateData).forEach((key) => {
+      if (updateData[key]) {
+        updates.push(`${key} = ?`);
+        values.push(updateData[key]);
+      }
+    });
+
+    query += updates.join(", ") + " WHERE emp_id = ?";
+    values.push(id);
+
+    return connection.promise().query(query, values);
+  }
 }
 
 module.exports = Employee;
