67cf0dfc9e841bc59fa058d4f7a0241cf38ceca5
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-12-06 09:50:22 -0500

    Fixed invoice to count number of nights the guest is staying

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -5,13 +5,24 @@ const invoiceView = (req, res) => {
   Invoice.findInvoiceById(id)
     .then(([rows]) => {
       if (rows.length > 0) {
+        const checkInDate = rows[0].guest_stay_check_in_date;
+        const checkOutDate = rows[0].guest_stay_check_out_date;
+
+        const date1 = new Date(checkInDate);
+        const date2 = new Date(checkOutDate);
+        const diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+        const roomPrice = parseFloat(rows[0].rm_base_rate * parseInt(diffDays));
+
         res.render("invoice", {
           pageTitle: "King William's - View Invoice",
           pageStyle: "/css/invoice.css",
           invoice: rows[0],
           transactions: rows,
           hasItems: rows[0].item_desc !== null,
-          roomPrice: rows[0].rm_base_rate,
+          roomPrice: roomPrice,
+          roomQuantity: `${diffDays} night(s)`,
         });
       } else {
         res
--- models/Invoice.js
+++ models/Invoice.js
@@ -13,7 +13,9 @@ class Invoice {
       item_desc,
       guest_trans_item_quantity,
       guest_trans_price,
-      room_rate.rm_base_rate
+      room_rate.rm_base_rate,
+      guest_stay.guest_stay_check_in_date,
+      guest_stay.guest_stay_check_out_date
       FROM guest_stay left join guest on guest_stay.guest_id=guest.guest_id 
       left join room on guest_stay.rm_id=room.rm_id
       left join room_category on room.rm_category_id=room_category.rm_category_id
